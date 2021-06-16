4848889ac83c99a5d66cf03c0140ca16d6885ae0
Ashan Shanaka <jpashanshanaka@gmail.com>
2021-06-16 11:49:05 +0530

    Folder structure for schemas

---
+++ models/Gig.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+// Create Schema
+const GigSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  details: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+mongoose.model('gigs', GigSchema);
