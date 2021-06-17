5006243da39be01141393e3392b0f2fee393a268
asinduvg <ashvg.dev@gmail.com>
2021-06-17 07:50:25 +0530

    user schema updated

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Backend API for mobile and web applications
 1. npm install
 2. add relevant credentials in 'config.env' file (create one in the root and add them)
 3. npm start
+
+# More
+config.env file should look like following,
+
+NODE_ENV=development
+PORT=5000
+DATABASE_LOCAL= add database connection string here
--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const tourRouter = require('./routes/tourRoutes');
 
 const app = express();
 
-// get access to the request body of a request object
-
 // MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // prints readable req messages in console
 }
+
+// get access to the request body of a request object
 app.use(express.json()); // body parser
 
 app.use((req, res, next) => {
