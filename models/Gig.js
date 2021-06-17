9db74ec67ad7d741df5a372bc4eda0781f7b4b85
Ashan Shanaka <jpashanshanaka@gmail.com>
2021-06-17 11:35:27 +0530

    on testing

--- models/Gig.js
+++ models/Gig.js
@@ -8,57 +8,57 @@ const GigSchema = new Schema({
     type: String,
     required: true,
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  minOrderAmount: {
-    type: Number,
-    required: true,
-  },
-  unit: {
-    type: String,
-    required: true,
-  },
-  unitPrice: {
-    type: Number,
-    required: true,
-  },
-  stock: {
-    type: Number,
-    required: true,
-  },
-  sold: {
-    type: Number,
-    required: true,
-  },
-  img: {
-    type: String,
-    required: true,
-  },
-  timeStamp: {
-    type: Date,
-    default: Date.now,
-  },
-  duration: {
-    type: Date,
-    required: true,
-  },
-  location: {
-    type: {
-      latitude: String,
-      longitude: String,
-    },
-    required: true,
-  },
-  growerId: {
-    type: String,
-    required: true,
-  },
+  // title: {
+  //   type: String,
+  //   required: true,
+  // },
+  // description: {
+  //   type: String,
+  //   required: true,
+  // },
+  // minOrderAmount: {
+  //   type: Number,
+  //   required: true,
+  // },
+  // unit: {
+  //   type: String,
+  //   required: true,
+  // },
+  // unitPrice: {
+  //   type: Number,
+  //   required: true,
+  // },
+  // stock: {
+  //   type: Number,
+  //   required: true,
+  // },
+  // sold: {
+  //   type: Number,
+  //   required: true,
+  // },
+  // img: {
+  //   type: String,
+  //   required: true,
+  // },
+  // timeStamp: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
+  // duration: {
+  //   type: Date,
+  //   required: true,
+  // },
+  // location: {
+  //   type: {
+  //     latitude: String,
+  //     longitude: String,
+  //   },
+  //   required: true,
+  // },
+  // growerId: {
+  //   type: String,
+  //   required: true,
+  // },
 });
 
 mongoose.model('gigs', GigSchema);
