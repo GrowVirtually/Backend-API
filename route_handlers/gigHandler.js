c1ecd0c4140ae6ed5bbe122bae64d65df3c0f599
Asindu <ashvg.dev@gmail.com>
2021-08-03 23:56:16 +0530

    nearest locations are rendering, but not working to distance

--- route_handlers/gigHandler.js
+++ route_handlers/gigHandler.js
@@ -1,52 +1,91 @@
-const { QueryTypes } = require('sequelize');
 const Gig = require('../models/Gig');
 const Location = require('../models/Location');
 const catchAsync = require('../utils/catchAsync');
 const sequelize = require('../models/db');
+const AppError = require('../utils/appError');
+const { QueryTypes } = require('sequelize');
 
 exports.createGig = catchAsync(async (req, res, next) => {
-  // add gig details to the table
-  let newGig = await Gig.create({
-    gigType: req.body.gigType,
-    gigCategory: req.body.gigCategory,
-    gigTitle: req.body.gigTitle,
-    gigDescription: req.body.gigDescription,
-    minOrderAmount: req.body.minOrderAmount,
-    unit: req.body.unit,
-    unitPrice: req.body.unitPrice,
-    stock: req.body.stock,
-    sold: req.body.sold,
-    gigDuration: Date.now(),
-    userid: 1,
-  });
+  // start the transaction
+  const t = await sequelize.transaction();
 
-  newGig = await newGig.save();
+  try {
+    // add gig details to the table
+    let newGig = await Gig.create(
+      {
+        gigType: req.body.gigType,
+        gigCategory: req.body.gigCategory,
+        gigTitle: req.body.gigTitle,
+        gigDescription: req.body.gigDescription,
+        minOrderAmount: req.body.minOrderAmount,
+        unit: req.body.unit,
+        unitPrice: req.body.unitPrice,
+        stock: req.body.stock,
+        sold: req.body.sold,
+        gigDuration: Date.now(),
+        userid: 1,
+      },
+      {
+        transaction: t,
+      }
+    );
 
-  // add locations to the table
-  await Promise.all(
-    req.body.locations.map(async (location) => {
-      await Location.create({
-        coordinates: sequelize.fn('ST_MakePoint', location.lat, location.lng),
-        gigId: newGig.gigId,
-      });
-    })
-  );
+    newGig = await newGig.save();
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      user: newGig,
-    },
-  });
+    // add locations to the table
+    await Promise.all(
+      req.body.locations.map(async (location) => {
+        await Location.create(
+          {
+            coordinates: sequelize.fn(
+              'ST_MakePoint',
+              location.lat,
+              location.lng
+            ),
+            gigId: newGig.gigId,
+          },
+          {
+            transaction: t,
+          }
+        );
+      })
+    );
+
+    await t.commit();
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user: newGig,
+      },
+    });
+  } catch (error) {
+    await t.rollback();
+    return next(new AppError('Transaction failed, data not inserted', 502));
+  }
 });
 
 exports.getAllGigs = catchAsync(async (req, res, next) => {
-  const result = await Gig.findAll();
+  const query = `SELECT "Gigs".*
+                 FROM (SELECT DISTINCT "gigId"
+                       FROM (SELECT "gigId"
+                             FROM "Locations"
+                             WHERE ST_DWithin(coordinates,
+                                              ST_MakePoint(${req.body.location.lat}, ${req.body.location.lng})::geography,
+                                              ${req.body.distance})
+                             ORDER BY coordinates <-> ST_MakePoint(${req.body.location.lat}, ${req.body.location.lng})::geography
+                             LIMIT ${req.body.limit}) AS nearGigIds) AS distinctGigIds
+                          INNER JOIN "Gigs"
+                                     ON distinctGigIds."gigId" = "Gigs"."gigId" `;
+
+  const gigs = await sequelize.query(query, {
+    type: QueryTypes.SELECT,
+  });
 
   res.status(201).json({
     status: 'success',
     data: {
-      gigs: result,
+      gigs,
     },
   });
 });
