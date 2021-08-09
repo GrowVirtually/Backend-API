1575cbb2bc3bda979bdba0be3a63076af90e06e8
Ashan Shanaka <44833876+ashanaka@users.noreply.github.com>
2021-08-09 09:59:27 +0530

    database refactor

---
+++ migrations/20210809025500-create-customer.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Customers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userid: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'userid',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Customers');
+  },
+};
---
+++ seeders/20210809041513-demo-consumer.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) =>
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    queryInterface.bulkInsert('Consumers', [
+      {
+        userid: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userid: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]),
+  down: async (queryInterface, Sequelize) =>
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    queryInterface.bulkDelete('Consumers', null, {}),
+};
