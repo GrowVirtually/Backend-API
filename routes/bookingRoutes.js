76c3c92483eed294744e4a99f7ac449502b2bb92
Asindu <ashvg.dev@gmail.com>
2021-09-16 22:37:50 +0530

    create order without webhooks done

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { protect } = require('../route_handlers/authHandler');
-const { getCheckoutSession } = require('../route_handlers/bookingHandler');
+const {
+  getCheckoutSession,
+  createOrderCheckout,
+} = require('../route_handlers/bookingHandler');
 
 const router = express.Router();
-router.use(protect);
+// router.use(protect);
 
-router.get('/checkout-session/:gigId/:units', getCheckoutSession);
+router.get('/checkout-session/:gigId/:units', protect, getCheckoutSession);
+router.get('/order-checkout', createOrderCheckout);
 module.exports = router;
--- routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createOrder } = require('../route_handlers/orderHandler');
-
-const router = express.Router();
-router.post('/', createOrder);
-
-module.exports = router;
