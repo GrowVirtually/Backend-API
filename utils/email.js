48a33134c866edd2d8456b936b301b0bbbd942df
Asindu <ashvg.dev@gmail.com>
2021-09-20 09:20:01 +0530

    email service updated

--- utils/email.js
+++ utils/email.js
@@ -12,23 +12,23 @@ module.exports = class Email {
 
   newTransport() {
     // if (process.env.NODE_ENV === 'production') {
-    return nodemailer.createTransport({
-      service: 'SendGrid',
-      auth: {
-        user: process.env.SENDGRID_USERNAME,
-        pass: process.env.SENDGRID_PASSWORD,
-      },
-    });
-    // }
-
     // return nodemailer.createTransport({
-    //   host: process.env.EMAIL_HOST,
-    //   port: process.env.EMAIL_PORT,
+    //   service: 'SendGrid',
     //   auth: {
-    //     user: process.env.EMAIL_USERNAME,
-    //     pass: process.env.EMAIL_PASSWORD,
+    //     user: process.env.SENDGRID_USERNAME,
+    //     pass: process.env.SENDGRID_PASSWORD,
     //   },
     // });
+    // // }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
   }
 
   // send the actual email
